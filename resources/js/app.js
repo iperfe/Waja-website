04c6ea9b992a21ddb144cc798d8304a5281bf94f
iperfe <168682611+iperfe@users.noreply.github.com>
2025-08-30 17:32:30 +0300

    dockerfix16

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 import './bootstrap';
 import 'vite/modulepreload-polyfill';
 import Alpine from 'alpinejs';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+window.swiper = Swiper;
 
 // Import Tailwind entry CSS
 import '../css/app.css';
